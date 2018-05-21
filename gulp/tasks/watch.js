efa403d2908c4cd17f41283edf0b53a87b7d8467
Егор Дьяченко <e.dyachenko@pugofka.com>
2018-05-21 16:20:32 +0300

    Правки структуры

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -1,12 +1,12 @@
-module.exports = function() {
+module.exports = function () {
   var p = global.p;
-  p.gulp.task("watch", function() {
+  p.gulp.task("watch", function () {
     p.gulp.watch(p.paths.watch.pug, p.gulp.series("pug"));
     p.gulp.watch(p.paths.watch.sass, p.gulp.series("sass"));
     p.gulp.watch(p.paths.watch.js, p.gulp.series("js"));
     p.gulp.watch(p.paths.watch.fonts, p.gulp.series("fonts"));
     p.gulp.watch(p.paths.watch.img, p.gulp.series("img"));
-    p.gulp.watch(p.paths.watch.svg, p.gulp.series("svg"));
+    p.gulp.watch(p.paths.watch.svg, p.gulp.series("svg", "pug"));
     p.gulp.watch(p.paths.watch.spriteSVG, p.gulp.series("svg-sprite"));
     p.gulp.watch(p.paths.watch.spritePNG, p.gulp.series("png-sprite"));
   });
