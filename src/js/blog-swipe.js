70460fc558bc3d45fe1846e962b74dd7e0eb66ec
Egor Dyachenko <whitebestnik@gmail.com>
2018-05-27 23:13:10 +0300

    Блог - сделал появление свайпа

---
+++ src/js/blog-swipe.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function() {
+  const blog = [];
+  const blogPage = document.querySelector(".blog");
+  if (blogPage) {
+    const blogSwipe = document.getElementById("jsSwipe");
+
+    const blogSwipeBtn = document.getElementById("jsSwipeBtn");
+    blog.scripts = function() {
+      blog.showSwipe = function() {
+        blogSwipeBtn.addEventListener("click", function(e) {
+          e.preventDefault();
+          blogSwipe.classList.add("blog-swipe--opened");
+        });
+      };
+      blog.showSwipe();
+    };
+    blog.scripts();
+  }
+});
