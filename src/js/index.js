a702b25b45def70004d3d91a2f26b13a85628550
Egor Dyachenko <whitebestnik@gmail.com>
2018-05-27 22:45:09 +0300

    Добавил flip-эффект на главной

---
+++ src/js/index.js
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded", function() {
+  const index = {};
+  index.script = function() {
+    const btnForToggleSide = document.getElementById("jsAuthBbtn");
+    const btnReturn = document.getElementById("jsAuthBtnReturn");
+    const personCard = document.getElementById("jsPersonCard");
+    const personCardSideFront = personCard.querySelector("#jsPersonCardFront");
+    const personCardSideBack = personCard.querySelector("#jsPersonCardBack");
+
+    index.toggleCardSide = function() {
+      btnForToggleSide.addEventListener("click", function(e) {
+        e.preventDefault();
+        btnForToggleSide.classList.add("welcome__auth-btn--hidden");
+        personCardSideFront.classList.add("jsToggle");
+        personCardSideBack.classList.remove("jsToggle");
+      });
+    };
+
+    index.jsAuthBtnReturn = function() {
+      btnReturn.addEventListener("click", function(e) {
+        btnForToggleSide.classList.remove("welcome__auth-btn--hidden");
+        personCardSideFront.classList.remove("jsToggle");
+        personCardSideBack.classList.add("jsToggle");
+      });
+    };
+
+    index.toggleCardSide();
+    index.jsAuthBtnReturn();
+  };
+
+  index.script();
+});
